7ad5b4d8b9219f931add2907a9ec0640a36d0cce
Dennis <devjam.ke@gmail.com>
2023-04-15 17:44:14 +0300

    Adding a Blog Post Show Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/blog_posts/:id", to: "blog_posts#show"
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
