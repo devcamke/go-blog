f4201c9a08caf27638543fa68bb04d2fde557e00
Dennis <devjam.ke@gmail.com>
2023-04-15 18:25:14 +0300

    Adding a New Blog Post Action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -9,4 +9,13 @@ class BlogPostsController < ApplicationController
     redirect_to root_path
   end
 
+def new
+  @blog_post = BlogPost.new
+
+end
+def create
+end
+private
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get "/blog_posts/new", to: "blog_posts#new", as: :blog_post_new
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
